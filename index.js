04170ef4e56cb3ae32e683e1ee07bae6a7dd623e
Stefania <126720391+Aoblu87@users.noreply.github.com>
2023-09-15 18:27:43 +0200

    update

--- index.js
+++ index.js
@@ -1,25 +1,54 @@
-window.onload= function(){
+window.onload = function () {
 
-    
+
     function displayMusicAlbum(result) {
-        console.log(result)
-        
-        const track = result.data[0]
-        const artist = track.artist
-        const album= track.album
-        console.log(artist.name)
 
-        document.querySelector('#artistName').innerText= artist.name
-        document.querySelector('#cover').src=album.cover_big
-        
+        const songArray = result.data
+        const imgTags = document.querySelectorAll('.cover')
+        const titles=document.querySelectorAll('.albumTitle')
+
+        for (let i = 0; i < songArray.length; i++) {
+
+            
+            // titolo artista
+            const artist = songArray[i].artist
+            document.querySelector('#artistName').innerText = artist.name
+
+            // Immagini cover
+            const album = songArray[i].album
+            for (const img of imgTags) {
+                img.src = album.cover_medium
+            }
+
+            // testo card overlay
+            const titleAlbum= album.title
+            for (const title of titles) {
+                ti
+            }
+
+
+
+        }
+
+        // console.log(songArray)
+        // const artist = track.artist
+        // const album= track.album
+        // console.log(artist.name)
+
+        // const artistName= document.querySelectorAll('.artistName').innerText= artist.name
+
+
+        // document.querySelector('#cover').src=album.cover_big
+
+
     }
-    
+
     function getRandomAlbum() {
         fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem")
-        .then(response => response.json())
-        .then(displayMusicAlbum)
+            .then(response => response.json())
+            .then(displayMusicAlbum)
     }
 
-getRandomAlbum()
+    getRandomAlbum()
 
 }
