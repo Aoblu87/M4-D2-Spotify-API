0f1ded6be9bc2cab5450480c4ed23f0e2dedfa10
Stefania <126720391+Aoblu87@users.noreply.github.com>
2023-09-16 12:14:39 +0200

    aggiunto modale manca funzione che legge input ricerca e lo inserisce nella fetch

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-// creo funzione per estrapolare la stringa dalla barra di ricerca e inserirla nel url della fetch
-
-const buttonSearch = document.querySelector('#button-search')
-const inputSearch = document.querySelector('#searchField')
-
-function readInputValue() {
-
-    let value = inputSearch.value
-
-    let url = "https://striveschool-api.herokuapp.com/api/deezer/search?q="
-
-    let fetch = url.concat('',value)
-
-}
 
 
 
 
 
-// --------------------------------------------------------
+// ---------funzione che mostra i risultati della chiamata----------
 function displayMusicAlbum(result) {
 
     const songArray = result.data
@@ -30,37 +16,53 @@ function displayMusicAlbum(result) {
         const arstistName = document.querySelector('#artistName')
         arstistName.innerText = songArray[i].artist.name
 
-        cardContainer.innerHTML += `
-        
-        <div class="card bg-transparent text-white border border-0 mb-4">
-        <img src="${songArray[i].album.cover_big}" class="cover card-img" alt="album cover">
-        <div class="card-img-overlay">
-        <h5 class="albumTitle" class="card-title"></h5>
-        <p class="card-text"></p>
-        <p class="card-text"></p>
-        </div>
-        </div>`
+        cardContainer.innerHTML += `<div class="card bg-transparent text-white border border-0 mb-4">
+                                        <img src="${songArray[i].album.cover_xl}" class="cover card-img" alt="album cover">
+                                        <div class="card-img-overlay">
+                                            <h5 class="albumTitle" class="card-title"></h5>
+                                            <p class="card-text"></p>
+                                            <p class="card-text"></p>
+                                        </div>
+                                    </div>`
 
 
+        // creo DOM per il modale
+        const modalList=document.querySelector('#albumTitle')
+
+        modalList.innerHTML += `<li class="list-group-item">${songArray[i].album.title}</li>`
     }
-
 }
 
 
 
-
-
-
-
-
-function getRandomAlbum(readInputValue) {
-
-    fetch(readInputValue)
+// ------------funzione di chiamata 
+function getRandomAlbum() {
+    // readInputValue()
+    fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=hisaishi')
+    
         .then(response => response.json())
         .then(displayMusicAlbum)
 }
+getRandomAlbum()
 
-// getRandomAlbum()
+
+// creo funzione per estrapolare la stringa dalla barra di ricerca e inserirla nel url della fetch
+
+// const buttonSearch = document.querySelector('#button-search')
+// const inputSearch = document.querySelector('#searchField')
 
 
+
+// function readInputValue() {
+//     let fetch = ''
+//     let input = 'ciao'
+
+//     let url = "https://striveschool-api.herokuapp.com/api/deezer/search?q="
+
+//      fetch = url.concat('', input)
+//      return fetch
+
+  
+
+// }
 
